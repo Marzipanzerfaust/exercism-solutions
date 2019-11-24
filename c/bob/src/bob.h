59cbf7fb94a21e7568e4e3d5fc880b828799d38b
Jeffrey Crochet <jeffrey.crochet@protonmail.com>
2019-11-24 15:56:03 -0600

    src/bob.h

--- c/bob/src/bob.h
+++ c/bob/src/bob.h
@@ -1,6 +1,14 @@
 #ifndef BOB_H
 #define BOB_H
 
-char *hey_bob(char *greeting);
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+char* hey_bob(const char[]);
+
+bool is_speech(const char[]);
+bool is_question(const char[]);
+bool is_shouting(const char[]);
 
 #endif
